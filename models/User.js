86b44177c6fbc8faed1e3c4e97cb63417bed3adb
Brennan Predmore <brennnann@gmail.com>
2020-10-23 23:57:46 -0500

    added comments in schema

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+// schema for the mongoDB database
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('user', UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 // @ROUTE POST api/users
 // @DESC Register a user
-// @ACCESS Public 
-router.post('/', (req, res) => {
-    res.send('Register a user');
-});
+// @ACCESS Public
+router.post(
+  '/',
+  [
+      check('name', 'Please add name')
+      .not()
+      .isEmpty(),
+      check('email', 'Please include a valid email'). isEmail(),
+      check('password', 'Please enter a passowrdr with 6 or more characters').isLength({min: 6})
+    ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('passed')
+  }
+);
 
 module.exports = router;
